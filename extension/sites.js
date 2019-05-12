193b5d2ea268f16fe4da930e85e3e995ba3df3c4
Tuur Lievens <tuur.lievens@hotmail.com>
2019-05-12 17:36:54 +0200

    More extension supported sites

--- extension/sites.js
+++ extension/sites.js
@@ -1,27 +1,26 @@
 /* eslint-disable quote-props */
 /* eslint-disable no-unused-vars */
 
+/**
+Example: (more info on https://tuur29.github.io/html2vr/library/)
+
+{
+  'url': '*://*.domain.com/*',
+  'properties': {
+    'page-type': 'grid',
+    'selector': '#nav a',
+  },
+  'settings': {
+    'columnCount': 2,
+  },
+  render: (settings) => {
+    return '<a-text value="hi" />';
+  },
+},
+
+*/
 const supportedURLs = [
-  {
-    'url': '*://*.tuurlievens.net/',
-    'properties': { // you can omit "data-html2vr-"
-      'page-type': 'grid',
-      'selector': '#nav a',
-    },
-    'settings': {
-      'columnCount': 2,
-    },
-  },
-  {
-    'url': '*://*.tuurlievens.net/home',
-    'properties': {
-      'page-type': 'grid',
-      'selector': '#nav a',
-    },
-    'settings': {
-      'columnCount': 2,
-    },
-  },
+  // part of Portfolio
   {
     'url': '*://*.tuurlievens.net/android',
     'properties': {
@@ -29,4 +28,31 @@ const supportedURLs = [
       'selector': '.card .photo img',
     },
   },
+
+  // Reddit
+  {
+    'url': '*://old.reddit.com/r/*/comments/*',
+    'properties': { // you can omit "data-html2vr-"
+      'page-type': 'detail',
+      'selector': 'a.title',
+    },
+    'render': (params) => {
+      const src = 'https:' + document.querySelector('.thing .thumbnail img').getAttribute('src');
+      return `
+        <a-image
+          class="html2vr-element clickable"
+          position="-1 1.5 -6"
+          width="1" height="1"
+          src="${src}" />
+      `;
+    },
+  },
+  {
+    'url': '*://old.reddit.com/*',
+    'properties': { // you can omit "data-html2vr-"
+      'page-type': 'grid',
+      'selector': '.thing .thumbnail img',
+    },
+  },
+
 ];
